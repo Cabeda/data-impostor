5459b8a0172dc9a6f2cb2f59a53647ee4e486b32
José Cabeda <jecabeda@gmail.com>
2024-02-01 15:37:48 +0000

    Add styles

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,11 +3,15 @@ import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
+	const posts = await getCollection("blog");
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
+		stylesheet: '/rss/styles.xsl',
+		xmlns: {
+			media: "http://search.yahoo.com/mrss/",
+		  },
 		items: posts.map((post) => ({
 			...post.data,
 			link: `/blog/${post.slug}/`,
